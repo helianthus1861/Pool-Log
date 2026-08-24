f29178178e7f56fb58ed75bbe0b9d31209925e63
helianthus1861 <helianthus1861@gmail.com>
2026-08-24 12:03:22 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE="pool-log-v3";
+const FILES=["./","./index.html","./manifest.json"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
